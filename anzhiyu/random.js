607d372e2f8ff19172ade4b39e6d70fdd8d735a2
unknown <2549251152@qq.com>
2024-04-15 02:04:34 +0800

    Site updated: 2024-04-15 02:04:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/15/hello-world/"];function toRandomPost(){
+var posts=["posts/helloWorld.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
