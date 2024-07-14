e7a792075d2d13243c62bf898eba4befb1e56514
Qiong Huang <2549251152@qq.com>
2024-07-14 18:29:30 +0800

    Site updated: 2024-07-14 18:29:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/helloWorld.html"];function toRandomPost(){
+var posts=["posts/helloWorld.html","posts/algorithm1.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
